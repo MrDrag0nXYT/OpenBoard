939a62201f3f71a1de4ab5832e8056e3d0299d4e
MrDrag0nXYT <147036304+MrDrag0nXYT@users.noreply.github.com>
2023-12-14 19:29:17 +0300

    Date/Time show + styles fix

--- src/main/java/zxc/solevoy/openboard/entity/Post.java
+++ src/main/java/zxc/solevoy/openboard/entity/Post.java
@@ -1,15 +1,12 @@
 package zxc.solevoy.openboard.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Entity
 @Table(name = "posts")
@@ -31,9 +28,28 @@ public class Post {
     @Column(name = "published_at")
     private LocalDateTime publishedAt;
 
+    @Transient
+    private String publishedAtFormatted;
+
     @Column(name = "edited_at")
     private LocalDateTime editedAt;
 
     @Column(name = "is_edited")
     private Boolean isEdited = false;
+
+
+    public String getPublishedAtFormatted() {
+
+        if (Duration.between(publishedAt, LocalDateTime.now()).getSeconds() < 60) {
+            return DateTimeFormatter.ofPattern("ss").format(publishedAt) + " секунд назад";
+        } else if (Duration.between(publishedAt, LocalDateTime.now()).getSeconds() >= 60
+                && Duration.between(publishedAt, LocalDateTime.now()).getSeconds() < 3599) {
+            return DateTimeFormatter.ofPattern("mm").format(publishedAt) + " минут назад";
+        } else if (Duration.between(publishedAt, LocalDateTime.now()).getSeconds() >= 3599
+                && Duration.between(publishedAt, LocalDateTime.now()).getSeconds() < 86459){
+            return DateTimeFormatter.ofPattern("HH").format(publishedAt) + " часов назад";
+        }
+
+        return DateTimeFormatter.ofPattern("HH:mm, dd MMMM, yyyy").format(publishedAt);
+    }
 }
